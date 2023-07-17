d496528fd86a008a7ae128ff6943b4cef9c85b1d
tarkIlJun <tack89826@gmail.com>
2023-07-17 16:48:25 +0900

    0717 - 오전 fileupload.

--- src/main/java/org/zerock/j2/dto/MemberCartDTO.java
+++ src/main/java/org/zerock/j2/dto/MemberCartDTO.java
@@ -1,10 +1,20 @@
 package org.zerock.j2.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class MemberCartDTO {
 
-    private Long cno;
+    private  Long cno;
 
-    private String email;
+    private  String email;
 
     private Long pno;
+
 }
